66ecb0e45c68da351d75bf381876f56e3ef634ef
vjai25 <virendra.jai25@gmail.com>
2025-01-13 16:28:11 +0530

    package jest and babel package updated

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders learn new react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/learn new react/i);
   expect(linkElement).toBeInTheDocument();
 });
